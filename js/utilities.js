5f509399441f945d8eb0ca60b5ca6d804401ba6a
Kazeeem <35298707+Kazeeem@users.noreply.github.com>
2019-02-11 00:57:53 +0100

    Updated Features

---
+++ js/utilities.js
@@ -0,0 +1,30 @@
+//=========== Preloader Script============
+
+    $(window).load(function() {
+        $("#loading").delay(350).fadeOut(350);
+    });
+
+//============ End of Preloader Script==============
+    
+//=========== Smooth scroll plugin initialiser=================
+
+	var scroll = new SmoothScroll('a[href*="#"]');
+
+//=========== End of Smooth scroll plugin initialiser=============
+
+//=========== Window scrolling function that changes the navigation bar background and shows the scroll to top button
+	$(window).on("scroll", function() {
+		if ($(window).scrollTop() > 700) { // if we've scrolled more than 700 pixels from top, change the background and display the scrolling button
+			$(".scroll-top").show();
+			$(".navbar").addClass("active");
+		} 
+		else {
+			$(".scroll-top").fadeOut().hide();
+			$(".navbar").removeClass("active");
+		}
+	});
+
+//=========== End of Window scrolling function that changes the navigation bar background and shows the scroll to top button
+
+//=========== Wow.js Animation Plugin Initialiser
+	new WOW().init();
